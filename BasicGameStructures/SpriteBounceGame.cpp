8c536f5c7572646a3ec13327720819162ac322de
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-09-16 02:55:06 +0430

    Initialize texture pointer in game constructor

--- BasicGameStructures/SpriteBounceGame.cpp
+++ BasicGameStructures/SpriteBounceGame.cpp
@@ -3,4 +3,11 @@
 SpriteBounceGame::SpriteBounceGame()
 {
 	m_window = &Window("First Game!", sf::Vector2u(400, 400));
+	m_texture = &sf::Texture();
+	
+}
+
+void SpriteBounceGame::HandleInput()
+{
+
 }
