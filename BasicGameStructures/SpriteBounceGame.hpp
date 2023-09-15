04e11d35f3ca86a962ba5d224a7a38c098bad309
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-09-16 02:38:25 +0430

    Design sprite bounce game class

---
+++ BasicGameStructures/SpriteBounceGame.hpp
@@ -0,0 +1,36 @@
+#ifndef SPRITE_BOUNCE_GAME_HPP
+#define SPRITE_BOUNCE_GAME_HPP
+
+#include "Window.hpp"
+#include <SFML/Graphics.hpp>
+
+class SpriteBounceGame
+{
+public:
+	SpriteBounceGame();
+	~SpriteBounceGame();
+
+	void HandleInput();
+	void Update();
+	void Render();
+
+
+private:
+
+	void setTransparency(sf::Color& l_color);
+	void SetTransparencySprite(sf::Sprite& l_sprite);
+
+	void SpriteHitBoundary_Y();
+	void SpriteHitBoundary_X();
+
+	void SpriteMove();
+
+	Window* m_window;
+	const float m_displacementX = 0.2f;
+	const float m_displacementY = 0.2f;
+	bool m_opaqueX = true;
+	bool m_opaqueY = true;
+};
+
+
+#endif
