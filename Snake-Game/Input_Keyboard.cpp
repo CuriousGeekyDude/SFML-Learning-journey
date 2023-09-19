925761c752f037c4a4cda2279ac86470eb684461
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-09-19 16:26:51 +0430

    Fix Game namespace

--- Snake-Game/Input_Keyboard.cpp
+++ Snake-Game/Input_Keyboard.cpp
@@ -1,60 +1,64 @@
-#include "Input_namespace.hpp"
+#include "Game_namespace.hpp"
 
-namespace Input 
-{
-	namespace Keyboard {
-		void MoveRight(sf::Transformable& l_transformable) {
-			l_transformable.move(sf::Vector2f(0.2f, 0.f));
-		}
+namespace Game{
 
-		void MoveLeft(sf::Transformable& l_transformable)
-		{
-			l_transformable.move(sf::Vector2f(-0.2f, 0.f));
-		}
+	namespace Input
+	{
+		namespace Keyboard {
 
-		void MoveUp(sf::Transformable& l_transformable)
-		{
-			l_transformable.move(sf::Vector2f(0.f, -0.2f));
-		}
-
-		void MoveDown(sf::Transformable& l_transformable)
-		{
-			l_transformable.move(sf::Vector2f(0.f, 0.2f));
-		}
-
-		sf::Keyboard::Key DetectArrowKey()
-		{
-			if (sf::Keyboard::isKeyPressed(Key::Right)) {
-				return Key::Right;
+			void MoveRight(sf::Transformable& l_transformable) {
+				l_transformable.move(sf::Vector2f(0.2f, 0.f));
 			}
-			else if (sf::Keyboard::isKeyPressed(Key::Left)) {
-				return Key::Left;
-			}
-			else if (sf::Keyboard::isKeyPressed(Key::Up)) {
-				return Key::Up;
-			}
-			else if (sf::Keyboard::isKeyPressed(Key::Down)) {
-				return Key::Down;
-			}
-		}
 
-		void HandleKeyboardInput(sf::Transformable& l_transformable)
-		{
-			switch (DetectArrowKey()) {
-			case sf::Keyboard::Right:
-				MoveRight(l_transformable);
-				break;
-			case sf::Keyboard::Left:
-				MoveLeft(l_transformable);
-				break;
-			case sf::Keyboard::Up:
-				MoveUp(l_transformable);
-				break;
-			case sf::Keyboard::Down:
-				MoveDown(l_transformable);
-				break;
+			void MoveLeft(sf::Transformable& l_transformable)
+			{
+				l_transformable.move(sf::Vector2f(-0.2f, 0.f));
 			}
-		}
 
+			void MoveUp(sf::Transformable& l_transformable)
+			{
+				l_transformable.move(sf::Vector2f(0.f, -0.2f));
+			}
+
+			void MoveDown(sf::Transformable& l_transformable)
+			{
+				l_transformable.move(sf::Vector2f(0.f, 0.2f));
+			}
+
+			sf::Keyboard::Key DetectArrowKey()
+			{
+				if (sf::Keyboard::isKeyPressed(Key::Right)) {
+					return Key::Right;
+				}
+				else if (sf::Keyboard::isKeyPressed(Key::Left)) {
+					return Key::Left;
+				}
+				else if (sf::Keyboard::isKeyPressed(Key::Up)) {
+					return Key::Up;
+				}
+				else if (sf::Keyboard::isKeyPressed(Key::Down)) {
+					return Key::Down;
+				}
+			}
+
+			void HandleKeyboardInput(sf::Transformable& l_transformable)
+			{
+				switch (DetectArrowKey()) {
+				case Key::Right:
+					MoveRight(l_transformable);
+					break;
+				case Key::Left:
+					MoveLeft(l_transformable);
+					break;
+				case Key::Up:
+					MoveUp(l_transformable);
+					break;
+				case Key::Down:
+					MoveDown(l_transformable);
+					break;
+				}
+			}
+
+		}
 	}
 }
