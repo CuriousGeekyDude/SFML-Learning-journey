83f951d5ed6cad1324174b9923b75c5ea23e0419
Farhan Aghnoum <130616138+CuriousGeekyDude@users.noreply.github.com>
2023-09-19 17:35:57 +0430

    Implement UpdatePosition()

--- Snake-Game/Apple.cpp
+++ Snake-Game/Apple.cpp
@@ -4,6 +4,12 @@ namespace Game
 {
 	namespace Objects
 	{
+
+		void Apple::UpdatePosition()
+		{
+			SetRandomPosition();
+		}
+
 		void Apple::SetRandomPosition()
 		{
 			m_apple.setPosition(sf::Vector2f(m_randomFloat.Generate(), m_randomFloat.Generate()));
